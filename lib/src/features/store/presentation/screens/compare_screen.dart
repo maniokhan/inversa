2bd134720da311a5c88a29d7f95b38b80ac76f4e
maniokhan <94386631+maniokhan@users.noreply.github.com>
2023-08-08 08:52:04 -0700

    work on compare_screens

--- lib/src/features/store/presentation/screens/compare_screen.dart
+++ lib/src/features/store/presentation/screens/compare_screen.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:inversaapp/src/common_widgets/common_app_bar.dart';
 import 'package:inversaapp/src/common_widgets/common_scaffold.dart';
 import 'package:inversaapp/src/constants/app_sizes.dart';
@@ -6,7 +8,10 @@ import 'package:inversaapp/src/features/store/presentation/widgets/new_list_tab_
 import 'package:inversaapp/src/features/store/presentation/widgets/old_list_tab_view.dart';
 import 'package:inversaapp/src/theme/config_colors.dart';
 
-class CompareScreen extends StatefulWidget {
+final newListProvider = StateProvider<List<Map<String, dynamic>>>((ref) => []);
+final oldListProvider = StateProvider<List<Map<String, dynamic>>>((ref) => []);
+
+class CompareScreen extends ConsumerStatefulWidget {
   static Route<CompareScreen> route() {
     return MaterialPageRoute(builder: (context) => const CompareScreen());
   }
@@ -14,16 +19,45 @@ class CompareScreen extends StatefulWidget {
   const CompareScreen({super.key});
 
   @override
-  State<CompareScreen> createState() => _CompareScreenState();
+  ConsumerState<CompareScreen> createState() => _CompareScreenState();
 }
 
-class _CompareScreenState extends State<CompareScreen>
+class _CompareScreenState extends ConsumerState<CompareScreen>
     with SingleTickerProviderStateMixin {
   late TabController tabController;
+
+  Future<void> fetchProducts(WidgetRef ref) async {
+    try {
+      QuerySnapshot<Map<String, dynamic>> snapshot =
+          await FirebaseFirestore.instance.collection('products').get();
+
+      DateTime currentTime = DateTime.now();
+      DateTime twentyFourHoursAgo =
+          currentTime.subtract(const Duration(hours: 24));
+
+      List<Map<String, dynamic>> fetchedProducts = snapshot.docs.map((doc) {
+        Map<String, dynamic> data = doc.data();
+        data['timestamp'] = (data['timestamp'] as Timestamp).toDate();
+        return data;
+      }).toList();
+
+      ref.read(newListProvider.notifier).update((state) => fetchedProducts
+          .where((product) => product['timestamp'].isAfter(twentyFourHoursAgo))
+          .toList());
+      ref.read(oldListProvider.notifier).update((state) => fetchedProducts
+          .where((product) => product['timestamp'].isBefore(twentyFourHoursAgo))
+          .toList());
+    } on FirebaseException catch (e) {
+      print("Error fetching data: $e");
+    }
+  }
 
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      fetchProducts(ref);
+    });
     tabController = TabController(length: 2, vsync: this);
   }
 
@@ -93,3 +127,46 @@ class _CompareScreenState extends State<CompareScreen>
     );
   }
 }
+
+
+  // List<Map<String, dynamic>> newList = [];
+  // List<Map<String, dynamic>> oldList = [];
+  // bool loading = false;
+
+  // Future<void> fetchProducts() async {
+  //   setState(() {
+  //     loading = true;
+  //   });
+  //   try {
+  //     QuerySnapshot<Map<String, dynamic>> snapshot =
+  //         await FirebaseFirestore.instance.collection('products').get();
+
+  //     DateTime currentTime = DateTime.now();
+  //     DateTime twentyFourHoursAgo =
+  //         currentTime.subtract(const Duration(hours: 24));
+
+  //     List<Map<String, dynamic>> fetchedProducts = snapshot.docs.map((doc) {
+  //       Map<String, dynamic> data = doc.data();
+  //       data['timestamp'] = (data['timestamp'] as Timestamp).toDate();
+  //       return data;
+  //     }).toList();
+
+  //     setState(() {
+  //       newList = fetchedProducts
+  //           .where(
+  //               (product) => product['timestamp'].isAfter(twentyFourHoursAgo))
+  //           .toList();
+  //       oldList = fetchedProducts
+  //           .where(
+  //               (product) => product['timestamp'].isBefore(twentyFourHoursAgo))
+  //           .toList();
+
+  //       loading = false;
+  //     });
+  //   } on FirebaseException catch (e) {
+  //     print("Error fetching data: $e");
+  //     setState(() {
+  //       loading = false;
+  //     });
+  //   }
+  // }
